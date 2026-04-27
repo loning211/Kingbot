22aa0e26b6dea89ccfd6f46a544b2424954dacac
loning211 <loningrony38@gmail.com>
2026-04-27 19:16:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print("--- Kingbot est en cours de démarrage ---")
+    bot_name = "Kingbot"
+    status = "Actif"
+    
+    print(f"Nom du bot : {bot_name}")
+    print(f"Statut : {status}")
+    print("Prêt à gérer les groupes !")
+
+if __name__ == "__main__":
+    main()
